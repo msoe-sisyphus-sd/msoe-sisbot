062c661e5dfd32c09c7379c8ce69e0ee65c1ac8d
Matthew Klundt <matt2@terribleproductions.com>
2017-05-08 15:07:42 -0500

    Update models. MK

--- sisbot/config.js
+++ sisbot/config.js
@@ -3,7 +3,7 @@ var config = {
 	receiver : true, // receive messages from cloud
 	sisbot_config : 'default.cson',
 	serial_path: '/dev/ttyACM0',
-	autoplay: false,
+	autoplay: true,
 	skip_incompatible: true,
 	max_rand_retries: 10,
 	check_internet_interval: 3600000 // once every hour, confirm an internet connection
