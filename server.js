736141a99afe7ac874247e6fd9993d043ec6afe0
Matthew Klundt <matt2@terribleproductions.com>
2017-05-09 14:54:39 -0500

    Config change. MK

--- server.js
+++ server.js
@@ -10,78 +10,88 @@ var _							= require('underscore');
 var exec 					= require('child_process').exec;
 // var httpProxy   = require('http-proxy');
 
-var config 				= require('./config.js');
+var local_config 				= require('./config.js');
 var ansible 		= require('./ansible.js');
 // var api 					= require('./api/server.js')(config, null);
 
-function getserial(){
-   var fs = require('fs');
-   var content = fs.readFileSync('/proc/cpuinfo', 'utf8');
-   var cont_array = content.split("\n");
-   var serial_line = cont_array[cont_array.length-2];
-   var serial = serial_line.split(":");
-   return serial[1].slice(1);
-}
-config.pi_serial = getserial();
+var config = {};
 
-/**************************** PROXY *******************************************/
+var app = function(given_config,ansible) {
+	_.extend(given_config, local_config);
 
-// var proxy       = httpProxy.createServer();
+	config = given_config;
 
-/**************************** SERVICES ****************************************/
+	function getserial(){
+	   var fs = require('fs');
+	   var content = fs.readFileSync('/proc/cpuinfo', 'utf8');
+	   var cont_array = content.split("\n");
+	   var serial_line = cont_array[cont_array.length-2];
+	   var serial = serial_line.split(":");
+	   return serial[1].slice(1);
+	}
+	config.pi_serial = getserial();
 
-// var used_ports = [];
-var services = {};
-console.log("Services:", config.services);
-_.each(config.services, function (service, key) {
-  if (service.address !== 'localhost') return this;
+	/**************************** PROXY *******************************************/
 
-	console.log("Create Service", key, service);
-  var service_obj = require(service.dir + '/'+key+'.js');
-	var local_config = require(service.dir + '/config.js');
-	local_config = _.extend(config, local_config);
+	// var proxy       = httpProxy.createServer();
 
-	services[key] = service_obj.init(local_config,ansible());
-});
+	/**************************** SERVICES ****************************************/
 
-/**************************** SERVER *******************************************/
+	// var used_ports = [];
+	var services = {};
+	console.log("Services:", config.services);
+	_.each(config.services, function (service, key) {
+	  if (service.address !== 'localhost') return this;
 
-var static             = new express();
+		console.log("Create Service", key, service);
+	  var service_obj = require(service.dir + '/'+key+'.js');
+		var local_config = require(service.dir + '/config.js');
+		local_config = _.extend(config, local_config);
 
-static.use(cors());
-static.use(bodyParser.json());
-static.use(bodyParser.urlencoded({ limit: '50mb' }));
-
-static.get('/', function(req, res) {
-	console.log("Get Page:",req.originalUrl);
-	iwconfig.status('wlan0', function(err, resp) {
-	  console.log("Status", resp);
-		if (resp.mode == 'master') {
-			res.sendFile(config.base_dir+'/index.html');
-		} else {
-			res.sendFile(config.base_dir+'/success.html');
-		}
+		services[key] = service_obj.init(local_config,ansible());
 	});
-});
-static.get('/*', function(req, res) {
-	console.log("Get:",req.originalUrl);
-  res.sendFile(config.base_dir+req.originalUrl);
-});
-static.post('/:service/:endpoint', function(req, res) {
-	service = req.params.service;
-	endpoint = req.params.endpoint;
 
-	var data = (_.isString(req.body.data)) ? JSON.parse(req.body.data) : req.body.data;
-	data = data.data;
+	/**************************** SERVER *******************************************/
 
-	console.log("Post:",service, endpoint, data);
+	var static             = new express();
 
-	var cb		= function (err, resp) {
-		res.json({ err: err, resp: resp });
-	};
-	services[service][endpoint](data,cb);
-});
+	static.use(cors());
+	static.use(bodyParser.json());
+	static.use(bodyParser.urlencoded({ limit: '50mb' }));
 
-http.createServer(static).listen(config.servers.sisbot.port);
+	static.get('/', function(req, res) {
+		console.log("Get Page:",req.originalUrl);
+		iwconfig.status('wlan0', function(err, resp) {
+		  console.log("Status", resp);
+			if (resp.mode == 'master') {
+				res.sendFile(config.base_dir+'/index.html');
+			} else {
+				res.sendFile(config.base_dir+'/success.html');
+			}
+		});
+	});
+	static.get('/*', function(req, res) {
+		console.log("Get:",req.originalUrl);
+	  res.sendFile(config.base_dir+req.originalUrl);
+	});
+	static.post('/:service/:endpoint', function(req, res) {
+		service = req.params.service;
+		endpoint = req.params.endpoint;
 
-console.log("Sisbot Server created");
+		var data = (_.isString(req.body.data)) ? JSON.parse(req.body.data) : req.body.data;
+		data = data.data;
+
+		console.log("Post:",service, endpoint, data);
+
+		var cb		= function (err, resp) {
+			res.json({ err: err, resp: resp });
+		};
+		services[service][endpoint](data,cb);
+	});
+
+	http.createServer(static).listen(config.servers.sisbot.port);
+
+	console.log("Sisbot Server created");
+}
+
+module.exports = app;
