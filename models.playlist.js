8c6015ed698727911899b3e3559d4a11aeeed24c
Matthew Klundt <matt2@terribleproductions.com>
2017-05-18 14:33:42 -0500

    Debug shuffle. MK

--- models.playlist.js
+++ models.playlist.js
@@ -50,10 +50,10 @@ var playlist = Backbone.Model.extend({
 	set_shuffle: function(value) {
 		console.log("Playlist set shuffle", value);
 		var randomized = this.get('is_shuffle');
-		if (value != randomized) {
-			this.set("is_shuffle", String(Boolean(value))); // set to "true" or "false"
+		if (String(value) != randomized) {
+			this.set("is_shuffle", String(value)); // set to "true" or "false"
 
-			if (value == "true") {
+			if (String(value) == "true") {
 				this._randomize();
 			} else {
 				var tracks = this.get("track_ids");
