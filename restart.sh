d3e7e1de658198d88e955394149f10251c941998
Matthew Klundt <matt2@terribleproductions.com>
2017-05-31 13:02:14 -0500

    Bugfix for restart. MK

--- restart.sh
+++ restart.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-echo "The following node processes were found:"
-ps aux | grep " node " | grep -v grep
-nodepids=$(ps aux | grep " node " | grep -v grep | cut -c10-15)
-echo "OK, so we will stop these process/es now..."
-for nodepid in ${nodepids[@]} do
-	echo "Stopping PID :"$nodepid
-	sudo kill -9 $nodepid
+#echo "The following node processes were found:"
+ps aux | grep " server.js" | grep -v grep
+nodepids=$(ps aux | grep " server.js" | grep -v grep | cut -c10-15)
+#echo "OK, so we will stop these process/es now..."
+for nodepid in ${nodepids[@]}
+do
+#echo "Stopping PID :"$nodepid
+sudo kill -9 $nodepid
 done
 
 cd /home/pi/sisbot-server/sisproxy
