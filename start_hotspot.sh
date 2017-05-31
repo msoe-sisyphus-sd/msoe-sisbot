5e45d5a0bfa004e3aba4bde6726dae3acee79794
Matthew Klundt <matt2@terribleproductions.com>
2017-05-31 16:08:11 -0500

    Make sure system services are updated before becoming hotspot. MK

--- start_hotspot.sh
+++ start_hotspot.sh
@@ -4,6 +4,7 @@ sudo cp /etc/network/interfaces.hotspot /etc/network/interfaces
 sudo ifup wlan0
 #sudo ifconfig wlan0 192.168.42.1
 
+sudo systemctl daemon-reload
 sudo service hostapd start
 sudo service isc-dhcp-server start
 
