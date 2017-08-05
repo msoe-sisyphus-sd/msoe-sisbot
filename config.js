3dcd69a63f49c3e955f083edd663912e6cd3860d
bshapiro57 <bshapiro57@users.noreply.github.com>
2017-08-05 14:20:30 -0500

    autodim connected to settings toggle

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV.indexOf('dev') == -1) {
 
 var config = {
 		base: {
-			version	: '0.5.16',
+			version	: '0.5.17',
 			debug   : false,
 			default_domain: 'sisyphus.local',
 			cert: function() {
