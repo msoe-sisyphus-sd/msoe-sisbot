83abc20e6df37bddd63c8cbd7536705d6509763f
bshapiro57 <bshapiro57@users.noreply.github.com>
2017-08-23 14:33:00 -0500

    set brightness = .5 on startup

--- sisbot.js
+++ sisbot.js
@@ -111,7 +111,7 @@ var sisbot = {
 				factory_resetting: "false",
 				factory_resetting_error: "",
 				installed_updates: "false",
-				brightness: 0.8,
+				brightness: 0.5,
 				speed: 0.35
 			});
 			this.current_state.set("local_ip", this._getIPAddress());
