660684b6757ee204bd605e8fb304f78a8294f3a2
Matthew Klundt <matt2@terribleproductions.com>
2017-09-27 13:53:59 -0500

    1.1.6b, another push test.

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV.indexOf('dev') == -1) {
 
 var config = {
 		base: {
-			version	: '1.1.5b',
+			version	: '1.1.6b',
 			debug   : false,
 			default_domain: 'sisyphus.local',
 			cert: function() {
