062c28c6b7688e7655c90cdd20b2c04832977636
Matthew Klundt <matt2@terribleproductions.com>
2017-10-23 13:09:21 -0500

    1.0.6, wifi auto-reconnect.

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV.indexOf('dev') == -1) {
 
 var config = {
 		base: {
-			version	: '1.2.0b',
+			version	: '1.0.6',
 			debug   : false,
 			default_domain: 'sisyphus.local',
 			cert: function() {
