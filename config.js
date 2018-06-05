c15c3ef93b921f68a8c7e9c2517481c598f52320
Matthew Klundt <matt2@terribleproductions.com>
2018-06-05 16:49:20 -0500

    1.5.2, Fix Max speed, attach/detach ball change

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV.indexOf('dev') == -1) {
 
 var config = {
 		base: {
-			version	: '1.5.1', // Max speed adjustable by cson
+			version	: '1.5.2', // Fix Max speed, attach/detach ball change
 			debug   : false,
 			default_domain: 'sisyphus.local',
 			cert: function() {
