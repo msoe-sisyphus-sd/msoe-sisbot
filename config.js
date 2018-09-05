64e32766361edc3b8ee57bdafa44e196a42b05e7
joel schaubert <joel.schaubert@gmail.com>
2018-09-05 05:54:40 -0500

    1.6.0 September 2018 release.

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV.indexOf('dev') == -1) {
 
 var config = {
 		base: {
-			version	: '1.5.10', // updates to CSON for attach path
+			version	: '1.6.0', // September 2018 release
 			debug   : false,
 			default_domain: 'sisyphus.local',
 			cert: function() {
