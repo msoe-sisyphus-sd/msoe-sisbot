e5ae3336909301de6f63fb803c490fc7f156d075
joel schaubert <joel.schaubert@gmail.com>
2018-10-23 15:12:20 -0500

    1.10.7  moveout after homing, security fixes for DNS rewrite attacks, modify 2 ball demo playlist tracks

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV.indexOf('dev') == -1) {
 
 var config = {
 		base: {
-			version	: '1.9.7', // detect host so we could cross check for dns rewrite attacks
+			version	: '1.10.7', // moveout after homing, security fixes from dns rewrite attacks
 			debug   : false,
 			default_domain: 'sisyphus.local',
 			cert: function() {
